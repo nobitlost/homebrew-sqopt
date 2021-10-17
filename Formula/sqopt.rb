d92c08df3e168faf3ceb3eb9eced199b6dacba65
dmelnichuk <39872028+dmelnichuk@users.noreply.github.com>
2021-10-17 00:30:37 +0000

    Update sqopt.rb

--- Formula/sqopt.rb
+++ Formula/sqopt.rb
@@ -1,4 +1,5 @@
-
+require "formula"
+require_relative "lib/private_strategy"
 
 class Sqopt < Formula
   include Language::Python::Virtualenv
@@ -8,12 +9,12 @@ class Sqopt < Formula
   version "0.4.1"
   bottle :unneeded
 
-  url "https://api.github.com/repos/nobitlost/squirrel_optimizer/releases/assets/47113444"
+  url "https://github.com/nobitlost/squirrel_optimizer/releases/download/0.4.1/sqopt-0.4.1-py3-none-any.whl", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "5f7fe529eade854794bb81b8040abdecd1ebc9115b044ba93fdddd129f7e0a53"
 
   depends_on "python@3.10"
   
   def install
+    virtualenv_install_with_resources
   end
-  
 end
